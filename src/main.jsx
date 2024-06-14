e5fbcdbb570834b9c6e2987574f00c4689ae55a1
thanhtrung <trungdtps27866@fpt.edu.vn>
2024-06-14 10:24:19 +0700

    commite 24/06/2024

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
-import store, { persistor } from "@redux/store/store.js";
+import store, { persistor } from "./redux/store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import LoadingView from "./pages/LoadingView.jsx";
 import { AppProvider } from "./context/AppContext.jsx";
