98d4fb7aefd74a6a0c130afa82b00108ea3ba91b
thanhtrung <trungdtps27866@fpt.edu.vn>
2024-06-14 10:57:02 +0700

    commit 14/06/2024

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/web-thuongmai/",
+  // base: "/web-thuongmai/",
   resolve: {
     alias: {
       "@actions": "/src/actions",
