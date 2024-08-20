0e2ddc1780831ff6797084ac8f2d9dca86a41d80
thanhtrung <trungdtps27866@fpt.edu.vn>
2024-08-20 18:04:54 +0700

    cmmit

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import { Provider } from "react-redux";
 import store, { persistor } from "./redux/store/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 import LoadingView from "./pages/LoadingView.jsx";
-import { AppProvider } from "./context/AppContext.jsx";
 import { injectStore } from "./api/api.js";
 injectStore(store);
 ReactDOM.createRoot(document.getElementById("root")).render(
